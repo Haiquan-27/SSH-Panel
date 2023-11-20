4ce49172ff08ac8603f48295e33e1f315e14339a
tai <tai@gameface.ai>
2023-11-20 14:43:19 +1100

    fix plugin exception if dependancy overwrite fails feat Main.sublime-menu

--- __init__.py
+++ __init__.py
@@ -9,18 +9,24 @@ except (ImportError,ModuleNotFoundError) as e:
         for item in os.listdir(src):
             s = os.path.join(src, item)
             d = os.path.join(dst, item)
-            if os.path.isdir(s):
-                if os.path.exists(d):
-                    shutil.rmtree(d)
-                shutil.copytree(s, d, symlinks, ignore)
-            else:
-                if os.path.exists(d):
-                    os.remove(d)
-                shutil.copy2(s, d)
+            try:
+                if os.path.isdir(s):
+                    if os.path.exists(d):
+                        shutil.rmtree(d)
+                    shutil.copytree(s, d, symlinks, ignore)
+                else:
+                    if os.path.exists(d):
+                        os.remove(d)
+                    shutil.copy2(s, d)
+            except (PermissionError, FileExistsError):
+                pass
     sublime_lib_p38 = os.path.join(sublime.packages_path(),'..','Lib','python38')
     copytree(os.path.join(os.path.dirname(__file__),'dependancies'), sublime_lib_p38)
     sublime_install_path = os.path.join(os.path.dirname(sys.executable),'python3.dll')
-    shutil.copy2(os.path.join(os.path.dirname(__file__),'python3.dll'), sublime_install_path)
+    try:
+        shutil.copy2(os.path.join(os.path.dirname(__file__),'python3.dll'), sublime_install_path)
+    except (PermissionError, FileExistsError):
+        pass
     try:
         import six,cffi,bcrypt,cryptography,pycparser,nacl,paramiko
     except (ModuleNotFoundError,ImportError):
